133b818a2f041e07e6f7869d8bd84963544d5dc7
Ralph Squillace <ralph@squillace.com>
2020-10-12 03:11:14 -0400

    cleaning up removed code and so on

--- cert-manager.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-export ns=$1
-  
-helm3 upgrade --install cert-manager --namespace $1 --version v0.16.1 --set installCRDs=true --set nodeSelector."beta\.kubernetes\.io/os"=linux jetstack/cert-manager --wait 
--- harbor-system-ingress.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-export ns=$1
-helm3 upgrade --install harbor-nginx-ingress ingress-nginx/ingress-nginx \
-    --namespace "$1" \
-    --set controller.ingressClass=harbor-nginx \
-    --set controller.replicaCount=2 \
-    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
-    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux
-
