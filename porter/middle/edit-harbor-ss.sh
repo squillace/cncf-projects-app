900084ad45b89cda068af5933763b694963220e0
ralph squillace <ralph@squillace.com>
2020-11-02 12:54:38 -0500

    fixing the stateful set whack

--- porter/middle/edit-harbor-ss.sh
+++ porter/middle/edit-harbor-ss.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl get statefulset -n harbor-system harbor-harbor-database -o json | jq --arg newCommand "chown -R postgres:postgres /var/lib/postgresql/data; chmod 700 -R /var/lib/postgresql/data" '.spec.template.spec.initContainers[].args[1]="$newCommand" | .spec.template.spec.initContainers[].args[0]="-c"' | kubectl apply -f -
+kubectl get statefulset -n harbor-system harbor-harbor-database -o json | jq --arg newCommand "chown -R postgres:postgres /var/lib/postgresql/data; chmod 700 -R /var/lib/postgresql/data" '.spec.template.spec.initContainers[0].args[1]=$newCommand' | kubectl apply -f -
+
+#kubectl get statefulset -n harbor-system harbor-harbor-database -o json | jq --arg newCommand "chown -R postgres:postgres /var/lib/postgresql/data; chmod 700 -R /var/lib/postgresql/data" '.spec.template.spec.initContainers[].args[1]="$newCommand" | .spec.template.spec.initContainers[].args[0]="-c"' | kubectl patch -f -
